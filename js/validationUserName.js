14e5c579ab976ddc87de5924ccd983870d0e51a9
AnniBaum <ann-christine.baum@outlook.com>
2017-06-13 16:05:06 +0200

    js
    geändert

---
+++ js/validationUserName.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Anni on 13.06.17.
+ */
+function userName_validation (username){
+
+    var username_length = username.value.length;
+    if (username != 0){
+
+        return true;
+
+    }
+
+}
