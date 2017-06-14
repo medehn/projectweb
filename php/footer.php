d3fe51cce6f990b826938bfc8a7cc01db572260e
Melanie Dehne <medehn@gmail.com>
2017-06-14 19:30:48 +0200

    code aufgeräumt

--- php/footer.php
+++ php/footer.php
@@ -1,12 +1,16 @@
-
 <!--Footer mit Facebook, FAQ, Impressum - wird in die normalen Seiten durch include eingebunden-->
 <footer class="container-fluid">
     <div class="row">
 
-        <p class="imprint"><button type="button" class="btn btn-default">Impressum</button></p>
-        <p class="faq"><button type="button" class="btn btn-default">FAQ</button></p>
-        <p class="facebook"><button type="button" class="btn btn-fb"><i class="fa fa-facebook left"></i> Facebook</button>
-            <a href="https://www.facebook.com/rabenbundzulandeck/" ></a>
+        <p class="imprint">
+            <button type="button" class="btn btn-default">Impressum</button>
+        </p>
+        <p class="faq">
+            <button type="button" class="btn btn-default">FAQ</button>
+        </p>
+        <p class="facebook">
+            <button type="button" class="btn btn-fb"><i class="fa fa-facebook left"></i> Facebook</button>
+            <a href="https://www.facebook.com/rabenbundzulandeck/"></a>
         </p>
 
     </div>
--- php/header.php
+++ php/header.php
@@ -4,9 +4,10 @@
     <div class="container-fluid">
 
         <div id="logo" class="col-sm-2 text-center">
-            <img class =logoImg src="img\logo.jpg" alt="Logo">
+            <img class=logoImg src="img\logo.jpg" alt="Logo">
         </div>
-        <div id = "header1" class="col-sm-10 text-center">
-            <img class =headerImg src="img\header.jpg" alt="Header">
+        <div id="header1" class="col-sm-10 text-center">
+            <img class=headerImg src="img\header.jpg" alt="Header">
         </div>
     </div>
+</div>
