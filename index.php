8083ab487436017ca2bcc20cadc2f8710c76ca9a
Melanie Dehne <medehn@gmail.com>
2017-06-14 19:29:33 +0200

    code aufgeräumt

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-<?php?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,7 +10,6 @@
 
 </head>
 <body>
-
 <?php
 include('php\nav.php');
 include('php\header.php')
@@ -22,17 +20,22 @@ include('php\header.php')
     <div class="row content">
         <div class="col-sm-2 sidenav">
             <p><a href="profile.php" id="navlink">Mein Profil</a></p>
-            <p><a href="events.php"  id="navlink">Anmeldung Lager</a></p>
+            <p><a href="events.php" id="navlink">Anmeldung Lager</a></p>
         </div>
         <div class="col-sm-6 text-left">
             <h1>Welcome</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+                ut aliquip ex ea commodo consequat.</p>
             <hr>
             <h3>Test</h3>
             <p>Lorem ipsum...</p>
         </div>
-        </div>
     </div>
+</div>
 <?php
 include('php\footer.php')
 ?>
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -25,91 +25,85 @@ include('php\header.php')
         <div class="col-sm-6 text-left">
             <div class="container">
                 <h1 class="well">Registrierung</h1>
-                <div class="col-lg-12 well">
-                    <div class="row">
-                        <form>
-                            <div class="col-sm-12">
-                                <div class="row">
-                                    <div class="col-sm-6 form-group">
-                                        <label>Vorname*</label>
-                                        <input type="text" placeholder="" class="form-control" required>
-                                    </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label>Nachname*</label>
-                                        <input type="text" placeholder="" class="form-control" required>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                <div class="col-sm-6 form-group">
-                                    <label>Strasse, Hausnummer</label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                                <div class="col-sm-6 form-group">
-                                    <label>PLZ</label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                                <div class="col-sm-6 form-group">
-                                    <label>Ort</label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label>Geburtsdatum</label>
-                                        <input type="date" placeholder="" class="form-control">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <label>Email Addresse*</label>
-                                    <input type="text" placeholder="" class="form-control" required>
-                                </div>
-                                    <br>
-                                    <hr>
-                                <div class="row">
-                                    <div class="col-sm-6 form-group">
-                                        <label>Username</label>
-                                        <input type="text" placeholder="" class="form-control" required>
-                                    </div>
-                                    <div class="col-sm-6 form-group">
-                                        <label>Passwort*</label>
-                                        <input type="text" placeholder="" class="form-control" required>
-                                    </div>
-                                </div>
+                <form>
+                    <div class="col-sm-12 well">
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Vorname*</label>
+                                <input type="text" placeholder="" class="form-control" required>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Nachname*</label>
+                                <input type="text" placeholder="" class="form-control" required>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Strasse, Hausnummer</label>
+                                <input type="text" placeholder="" class="form-control">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>PLZ</label>
+                                <input type="text" placeholder="" class="form-control">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Ort</label>
+                                <input type="text" placeholder="" class="form-control">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Geburtsdatum</label>
+                                <input type="date" placeholder="" class="form-control">
+                            </div>
+                            <div class="col-sm-6 form-group">
+                            <label>Email Addresse*</label>
+                            <input type="text" placeholder="" class="form-control" required>
+                            </div>
+                            <br>
+                            <hr>
+                            <div class="col-sm-6 form-group">
+                                <label>Username</label>
+                                <input type="text" placeholder="" class="form-control" required>
+                            </div>
+                            <div class="col-sm-6 form-group">
+                                <label>Passwort*</label>
+                                <input type="text" placeholder="" class="form-control" required>
+                            </div>
+                        </div>
 
+                        <br>
+                        <hr>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Funktion im Verein*</label>
+                                <input type="text" placeholder="" class="form-control" required>
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <div class="col-sm-6 form-group">
+                                <label>Fähigkeiten</label>
+                                <input type="text" placeholder="" class="form-control">
+                            </div>
+
+                            <div class="col-sm-6 form-group">
+                                <label>Interessen</label>
+                                <input type="text" placeholder="" class="form-control">
                                 <br>
                                 <hr>
-                                <div class="row">
-                                    <div class="col-sm-6 form-group" >
-                                        <label>Funktion im Verein*</label>
-                                        <input type="text" placeholder="" class="form-control" required>
-                                    </div>
-
-                                </div>
-                                <div class="row">
-                                <div class="col-sm-6 form-group" >
-                                    <label>Fähigkeiten</label>
-                                    <input type="text" placeholder="" class="form-control">
-                                </div>
-
-                                <div class="col-sm-6 form-group" >
-                                    <label>Interessen</label>
-                                    <input type="text" placeholder="" class="form-control">
-                                <br>
-                                    <hr>
-                                </div>
-
-                                    <div class="container">
-                                        <div class="row">
-                                            <h2>  <div class="text-center">
-                                                    <button type="button" class="btn btn-labeled btn-success">
-                                                        <span class="btn-label"><i class="glyphicon glyphicon-ok"></i></span>Absenden</button>
-
-                                                    <br /></h2>
-                                        </div>
-                                    </div>
                             </div>
+
+                            <div class="container">
+                                <div class="row">
+                                    <h2>
+                                        <div class="text-center">
+                                            <button type="button" class="btn btn-labeled btn-success">
+                                                        <span class="btn-label"><i
+                                                                    class="glyphicon glyphicon-ok"></i></span>Absenden
+                                            </button>
+                                    </h2>
+                                </div>
                             </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
