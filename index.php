83cd5414eb6cfc47a43ac9e560108215ad29aec4
Melanie Dehne <medehn@gmail.com>
2017-06-15 09:19:53 +0200

    fix

--- index.php
+++ index.php
@@ -31,8 +31,6 @@ include('php\header.php')
                 labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
                 ut aliquip ex ea commodo consequat.</p>
             <hr>
-            <h3>Test</h3>
-            <p>Lorem ipsum...</p>
         </div>
     </div>
 </div>
--- projectweb-master/README.md
+++
@@ -1,4 +0,0 @@
-# projectweb
-FH Kufstein - Web Developement - project repository
-
-Task in course "Web Developement" - creating a web application/website with functionality in groups
