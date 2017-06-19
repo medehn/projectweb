95777c30ed892aa51ed4457bba4f800272777d7b
Melanie Dehne <medehn@gmail.com>
2017-06-19 18:57:26 +0200

    reg register angefangen

--- register.php
+++ register.php
@@ -12,46 +12,46 @@ include('php\header.php')
         <div class="col-sm-6 text-left">
             <div class="container">
                 <h1 class="well">Registrierung</h1>
-                <form>
+                <form action="reg.php" method="post">
                     <div class="col-sm-12 well">
                         <div class="row">
                             <div class="col-sm-6 form-group">
                                 <label>Vorname*</label>
-                                <input type="text" placeholder="" class="form-control" required>
+                                <input type="text" placeholder="" name="fname" class="form-control" required>
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>Nachname*</label>
-                                <input type="text" placeholder="" class="form-control" required>
+                                <input type="text" placeholder="" name="lname" class="form-control" required>
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>Strasse, Hausnummer</label>
-                                <input type="text" placeholder="" class="form-control">
+                                <input type="text" placeholder="" name="adresse" class="form-control">
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>PLZ</label>
-                                <input type="text" placeholder="" class="form-control">
+                                <input type="text" placeholder="" name="plz"class="form-control">
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>Ort</label>
-                                <input type="text" placeholder="" class="form-control">
+                                <input type="text" placeholder="" name="ort" class="form-control">
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>Geburtsdatum</label>
-                                <input type="date" placeholder="" class="form-control">
+                                <input type="date" placeholder="" name="gebdatum"class="form-control">
                             </div>
                             <div class="col-sm-6 form-group">
                             <label>Email Addresse*</label>
-                            <input type="text" placeholder="" class="form-control" required>
+                            <input type="text" placeholder="" name="email" class="form-control" required>
                             </div>
                             <br>
                             <hr>
                             <div class="col-sm-6 form-group">
                                 <label>Username</label>
-                                <input type="text" placeholder="" class="form-control" required>
+                                <input type="text" placeholder="" name="username" class="form-control" required>
                             </div>
                             <div class="col-sm-6 form-group">
                                 <label>Passwort*</label>
-                                <input type="text" placeholder="" class="form-control" required>
+                                <input type="text" placeholder=""  class="form-control" required>
                             </div>
                         </div>
 
@@ -60,19 +60,19 @@ include('php\header.php')
                         <div class="row">
                             <div class="col-sm-6 form-group">
                                 <label>Funktion im Verein*</label>
-                                <input type="text" placeholder="" class="form-control" required>
+                                <input type="text" placeholder="" name="funktion" class="form-control" required>
                             </div>
 
                         </div>
                         <div class="row">
                             <div class="col-sm-6 form-group">
                                 <label>Fähigkeiten</label>
-                                <input type="text" placeholder="" class="form-control">
+                                <input type="text" placeholder=""  class="form-control">
                             </div>
 
                             <div class="col-sm-6 form-group">
                                 <label>Interessen</label>
-                                <input type="text" placeholder="" class="form-control">
+                                <input type="text" placeholder=""   class="form-control">
                                 <br>
                                 <hr>
                             </div>
