07cb4474d3047727aa808e572d0c4503a6ff74ee
Melanie Dehne <medehn@gmail.com>
2017-06-19 16:51:07 +0200

    db
    db details und connection

---
+++ database/dbNewConnection.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'dbDetails.php';
+
+$connection = mysqli_connect($server, $user, $pwd, $db) or die('Verbindung zu Server ' . $server . ' konnte nicht aufgebaut werden.');
+
+echo '<p>Successfuly connected to server ' . $server . '</p>';
+
+?>
