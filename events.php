f62e87a92a710e6d80b3cc062f64556b2e48ef1a
Melanie Dehne <medehn@gmail.com>
2017-06-20 11:53:05 +0200

    events fix

--- events.php
+++ events.php
@@ -1,10 +1,8 @@
 <!-- Seite zur Anmeldung für Events-->
 <?php
-<<<<<<< HEAD
+
 include('php\nav.php'); ?>
-=======
-include('php/nav.php');?>
->>>>>>> 787039eed80a99e7a855e8745378617b43d8fa5e
+
 <ol class="breadcrumb">
     <li class="active">Home</li>
     <li><a href="#">Anmeldung Events</a></li>
@@ -29,144 +27,127 @@ include('php/header.php')
 
 <<<<<<< HEAD
                     <!-- Name-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="name">Name</label>
-                        <div class="col-md-4">
-                            <input id="name" name="fname" type="text" placeholder="Name" class="form-control input-md"
-                                   required="">
-=======
-                        <!-- Name-->
-                        <form action="php/eintragen.php" method="post">
+                    <form action="php/eintragen.php" method="post">
                         <div class="form-group">
                             <label class="col-md-4 control-label" for="name">Name</label>
                             <div class="col-md-4">
-                                <input id="name" name="fname" type="text" placeholder="Name" class="form-control input-md" required="">
+                                <input id="name" name="fname" type="text" placeholder="Name"
+                                       class="form-control input-md" required="">
                             </div>
->>>>>>> 787039eed80a99e7a855e8745378617b43d8fa5e
-                        </div>
-                    </div>
 
-                    <!-- Email Adresse-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="emailaddress">Email Addresse</label>
-                        <div class="col-md-4">
-                            <input id="emailaddress" name="emailaddress" type="text" placeholder="Email Adresse"
-                                   class="form-control input-md" required="">
                         </div>
 
-                        <!-- Event-->
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Event</label>
-                        <div class="col-md-4">
-                            <input type="text" placeholder="Event (Ehrenberg, Schluderns, ..."
-                                   class="form-control input-md" required="">
-                        </div>
-                    </div>
 
-                    <!-- Wieviele Tage? -->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Tage</label>
-                        <div class="col-md-4">
-                            <select class="form-control">
-                                <option value="1">2</option>
-                                <option value="2">3</option>
-                                <option value="3">4</option>
-                            </select>
-                        </div>
-                    </div>
-
-                    <!-- Eigenes Zelt?-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Eigenes Zelt</label>
-                        <div class="col-md-4">
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" value="1" checked="checked">
-                                    Ja
-                                </label>
+                        <!-- Email Adresse-->
+                        <div class="form-group">
+                            <div class="col-md-4">
+                                <input id="emailaddress" name="emailaddress" type="text" placeholder="Email Adresse"
+                                       class="form-control input-md" required="">
                             </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" value="2">
-                                    Nein
-                                </label>
+
+                            <!-- Event-->
+                        </div>
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Event</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="Event (Ehrenberg, Schluderns, ..."
+                                       class="form-control input-md" required="">
                             </div>
                         </div>
-                    </div>
 
-                    <!-- Eigenes Auto?-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Eigenes Auto</label>
-                        <div class="col-md-4">
-                            <div class="radio">
-                                <label">
-                                <label>
-                                    <input type="radio" value="1" checked="checked">
-                                    Ja
-                                </label>
-                            </div>
-                            <div class="radio">
-                                <label>
-                                    <input type="radio" value="2">
-                                    Nein
-                                </label>
+                        <!-- Wieviele Tage? -->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Tage</label>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option value="1">2</option>
+                                    <option value="2">3</option>
+                                    <option value="3">4</option>
+                                </select>
                             </div>
                         </div>
-                    </div>
-                    <!-- Eigenes Auto-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label">Platz im Auto für wieviele Personen?</label>
-                        <div class="col-md-4">
-                            <select class="form-control">
-                                <option value="2">0</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="2">3</option>
-                                <option value="2">mehr</option>
-                            </select>
-                        </div>
-                    </div>
 
-                    <!-- Auto-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label"> Bei Auto: zB Anhängerkupplung, Platz für Equipment,
-                            Mitfahrer....</label>
-                        <div class="col-md-4">
-                            <input type="text" placeholder="" class="form-control input-md">
-                        </div>
-                    </div>
-
-<<<<<<< HEAD
-                    <!-- Sonstiges-->
-                    <div class="form-group">
-                        <label class="col-md-4 control-label" for="hotel">Infos, Wünsche, Anmerkungen?</label>
-                        <div class="col-md-4">
-                            <input type="text" placeholder="" class="form-control input-md">
-=======
+                        <!-- Eigenes Zelt?-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Eigenes Zelt</label>
+                            <div class="col-md-4">
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" value="1" checked="checked">
+                                        Ja
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" value="2">
+                                        Nein
+                                    </label>
                                 </div>
                             </div>
-                    </fieldset>
-                    <div class="container">
-                        <div class="row">
-                            <h2>  <div class="text-center">
-                                    <button type="submit" name="submit" class="btn btn-labeled btn-success">
-                                        <span class="btn-label"><i class="glyphicon glyphicon-ok"></i></span>Absenden</button>
->>>>>>> 787039eed80a99e7a855e8745378617b43d8fa5e
-
                         </div>
-                    </div>
+
+                        <!-- Eigenes Auto?-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Eigenes Auto</label>
+                            <div class="col-md-4">
+                                <div class="radio">
+                                    <label">
+                                    <label>
+                                        <input type="radio" value="1" checked="checked">
+                                        Ja
+                                    </label>
+                                </div>
+                                <div class="radio">
+                                    <label>
+                                        <input type="radio" value="2">
+                                        Nein
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Eigenes Auto-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label">Platz im Auto für wieviele Personen?</label>
+                            <div class="col-md-4">
+                                <select class="form-control">
+                                    <option value="2">0</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                    <option value="2">3</option>
+                                    <option value="2">mehr</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <!-- Auto-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label"> Bei Auto: zB Anhängerkupplung, Platz für Equipment,
+                                Mitfahrer....</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="" class="form-control input-md">
+                            </div>
+                        </div>
+
+
+                        <!-- Sonstiges-->
+                        <div class="form-group">
+                            <label class="col-md-4 control-label" for="hotel">Infos, Wünsche, Anmerkungen?</label>
+                            <div class="col-md-4">
+                                <input type="text" placeholder="" class="form-control input-md">
+
+                            </div>
+                        </div>
                 </fieldset>
                 <div class="container">
                     <div class="row">
                         <h2>
                             <div class="text-center">
-                                <button type="button" class="btn btn-labeled btn-success">
+                                <button type="submit" name="submit" class="btn btn-labeled btn-success">
                                     <span class="btn-label"><i class="glyphicon glyphicon-ok"></i></span>Absenden
                                 </button>
 
-                                <br/>
-                        </h2>
+
+                            </div>
                     </div>
                 </div>
             </form>
