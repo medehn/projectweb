07e839575f2930a53e227ede0ce83d4a10b04011
Melanie Dehne <medehn@gmail.com>
2017-06-23 11:46:35 +0200

    head gefixt

--- php/nav.php
+++ php/nav.php
@@ -1,4 +1,17 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
 
+    <!--mobile Ansicht-->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rabenbund zu Landeck</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+
+</head>
 
 <!-- Navigation - wird in die normalen Seiten durch include eingebunden-->
 <nav class="navbar navbar-inverse">
