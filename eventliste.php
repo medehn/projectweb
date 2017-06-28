945ac14070159109262485ae3f368f8a4ee03acc
Melanie Filipovic <stud.melanie.filipovic@fh-kufstein.ac.at>
2017-06-28 16:14:01 +0200

    Listen geupdated

--- eventliste.php
+++ eventliste.php
@@ -2,7 +2,7 @@
 include('php'.DIRECTORY_SEPARATOR.'nav_logout.php'); ?>
 <ol class="breadcrumb">
     <li class="active">Home</li>
-    <li><a href="#">Mitglieder</a></li>
+    <li><a href="#">Anstehende Events</a></li>
 </ol>
 
 <?php
@@ -26,8 +26,8 @@ include('php'.DIRECTORY_SEPARATOR.'header.php')
 
             include(__DIR__.'/database/dbNewConnection.php');
 
-            $query = "SELECT fname,lname,birthdate,plz,ort
-          FROM benutzer;";
+            $query = "SELECT eventname, tage, zelt, sonstiges
+          FROM events;";
 
 
 
@@ -47,7 +47,7 @@ include('php'.DIRECTORY_SEPARATOR.'header.php')
             <section>
                 <div class="container-fluid text-center">
 
-                    <h3>Liste der Mitglieder des Vereins</h3>
+                    <h3>Liste der Anstehenden Events</h3>
                     <ul>
 
 
@@ -60,37 +60,32 @@ include('php'.DIRECTORY_SEPARATOR.'header.php')
                         $db_link = mysqli_connect('127.0.0.1', 'root','','rabenbund');
 
                         $db_erg = mysqli_query( $db_link, $query );
-                         if ( ! $db_erg )
-                         {
-                             die('Ungültige Abfrage: ' . mysqli_error(mysqli_connect('127.0.0.1','root','','rabenbund')));
-                         }
+                        if ( ! $db_erg )
+                        {
+                            die('Ungültige Abfrage: ' . mysqli_error(mysqli_connect('127.0.0.1','root','','rabenbund')));
+                        }
 
-                         echo '<table border="1">';
-                         while ($zeile = mysqli_fetch_array( $db_erg, 1))
-                         {
-                             echo "<tr>";
-                             //echo "<td>". $zeile['userid'] . "</td>";
-                             echo "<td>". $zeile['fname'] . "</td>";
-                             echo "<td>". $zeile['lname'] . "</td>";
-                             echo "<td>". $zeile['birthdate'] . "</td>";
-                             //echo "<td>". $zeile['email'] . "</td>";
-                             //echo "<td>". $zeile['adress'] . "</td>";
-                             //echo "<td>". $zeile['anumber'] . "</td>";
-                             echo "<td>". $zeile['plz'] . "</td>";
-                             echo "<td>". $zeile['ort'] . "</td>";
-                             echo "</tr>";
-                         }
-                         echo "</table>";
+                        echo '<table border="1">';
+                        while ($zeile = mysqli_fetch_array( $db_erg, 1))
+                        {
+                            echo "<tr>";
+                            echo "<td>". $zeile['eventname'] . "</td>";
+                            echo "<td>". $zeile['tage'] . "</td>";
+                            echo "<td>". $zeile['zelt'] . "</td>";
+                            echo "<td>". $zeile['sonstiges'] . "</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
 
-                         mysqli_free_result( $db_erg );
+                        mysqli_free_result( $db_erg );
 
 
 
                         //            if ($results->num_rows > 0) {
-                        //                echo "<table border =1><tr><th>Vorname   </th><th>Nachname   </th><th>Geburtstag   </th><th>PLZ   </th><th>Ort   </th></tr>";
+                        //                echo "<table border =1><tr><th>Eventname   </th><th>Tage   </th><th>Zelt   </th><th>Sonstiges   </th></tr>";
                         //                // output data of each row
                         //                while ($row = $results->fetch_assoc()) {
-                        //                    echo "<tr><td>" . $row["fname"] . "</td><td>" . $row["lname"] . "</td><td>" . $row["birthdate"]  . "</td><td>"  . $row["plz"]  ."</td><td>"  . $row["ort"]  ."</td></tr>";
+                        //                    echo "<tr><td>" . $row["eventname"] . "</td><td>" . $row["tage"] . "</td><td>" . $row["zelt"]  . "</td><td>" . $row["sonstiges"]  . "</td></tr>";
                         //                }
                         //                echo "</table>";
                         //            } else {
@@ -105,9 +100,9 @@ include('php'.DIRECTORY_SEPARATOR.'header.php')
             </section>
             </body>
             </html>
-<!--            <h3 class="text-center">UNDER CONSTRUCTION</h3>-->
-<!---->
-<!--            <img class="text-center" src="img\knight.png">-->
+            <!--            <h3 class="text-center">UNDER CONSTRUCTION</h3>-->
+            <!---->
+            <!--            <img class="text-center" src="img\knight.png">-->
         </div>
     </div>
 </div>
