af8250a47137d5a1b1c58a3fed0a8f723a1e71de
RobertFitzko <robertfitzko@gmx.net>
2017-06-29 16:59:38 +0200

    ups kleiner Denkfehler

--- php/logout.php
+++ php/logout.php
@@ -2,6 +2,6 @@
 <?php
 session_start();
 session_destroy();
-header('location: ../index.php');
+header('location: Startseite.php');
 
 ?>
--- php/nav_logout.php
+++ php/nav_logout.php
@@ -77,7 +77,7 @@
                             <?php } ?>
                             <h1>Ausloggen</h1><br>
                             <!-- Auslagvorgang findet hier statt-->
-                            <form method="post" class="form-signin" id="logout" action="index.php">
+                            <form method="post" class="form-signin" id="logout" action="php/logout.php">
                                 <input type="text" name="username" id="username" placeholder="Username">
                                 <input type="password" name="userpassword" id="userpassword" placeholder="Password">
                                 <input type="submit" name="Submit" class="login loginmodal-submit"
