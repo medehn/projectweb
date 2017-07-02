189a67616dfdf1e7472db2cd2faf72c29b4e2f91
RobertFitzko <robertfitzko@gmx.net>
2017-07-02 18:16:44 +0200

    php

--- profilbearbeiten.php
+++ profilbearbeiten.php
@@ -25,17 +25,7 @@ if (isset($_SESSION["username"])) {
 
     $results = mysqli_query($connection, $query) or die('Abfrage konnte nicht verarbeitet werden');
 
-<<<<<<< HEAD
 
-
-=======
-    $fname = htmlentities($_POST['fname'], ENT_QUOTES, 'utf-8');
-    $lname = $_POST['lname'];
-    $username = $_POST['username'];
-    $funktion = $_POST['funktion'];
-    $skills = $_POST['skills'];
-    $interests = $_POST['interests'];
->>>>>>> origin/master
 
     if (isset($fname)) {
         echo "FName geht";
